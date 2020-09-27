30b9cc77ab8a8b4676092bd5c1d516f987afecd7
waynesg <waynesg511@gmail.com>
2020-09-27 10:07:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,9 +23,12 @@ git clone https://github.com/waynesg/luci-app-pptp-server.git package/pptp
 # 获取luci-app-oaf
 git clone https://github.com/destan19/OpenAppFilter.git package/oaf
 # 获取luci-app-ipsec
-# git clone https://github.com/waynesg/luci-app-ipsec-vpnserver-manyusers.git package/ipsec
+git clone https://github.com/waynesg/luci-app-ipsec-vpnserver-manyusers.git package/ipsec
 # 获取luci-app-argon-config
 git clone https://github.com/waynesg/luci-app-argon-config.git package/argon-config
+# 获取hello world和依赖
+git clone https://github.com/jerrykuku/lua-maxminddb package/diy-packages/helloworld/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr package/diy-packages/helloworld/luci-app-vssr
 # 清除默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #=================================================
