51a55ec2805405018cae1b2978862fcafb571639
waynesg <waynesg511@gmail.com>
2020-11-29 20:55:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,8 +27,8 @@ git clone https://github.com/destan19/OpenAppFilter.git package/oaf
 # 获取luci-app-argon-config
 git clone https://github.com/waynesg/luci-app-argon-config.git package/argon-config
 # 获取hello world和依赖
-git clone https://github.com/jerrykuku/lua-maxminddb package/diy-packages/helloworld/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr package/diy-packages/helloworld/luci-app-vssr
+# git clone https://github.com/jerrykuku/lua-maxminddb package/diy-packages/helloworld/lua-maxminddb
+# git clone https://github.com/jerrykuku/luci-app-vssr package/diy-packages/helloworld/luci-app-vssr
 # 清除默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #=================================================
