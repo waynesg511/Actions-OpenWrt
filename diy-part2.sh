9aa89872482fe2ce9b7729a0e2516b3655285b70
Wayne Zhong <waynesg511@gmail.com>
2021-01-27 10:06:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,7 +26,7 @@ git clone https://github.com/waynesg/luci-app-poweroff.git package/poweroff
 # 获取luci-app-pptp-server
 git clone https://github.com/waynesg/luci-app-pptp-server.git package/pptp
 # 获取luci-app-oaf
-# git clone https://github.com/destan19/OpenAppFilter.git package/oaf
+git clone https://github.com/destan19/OpenAppFilter.git package/oaf
 # 获取luci-app-ipsec
 # git clone https://github.com/waynesg/luci-app-ipsec-vpnserver-manyusers.git package/luci-app-ipsec-vpnserver-manyusers
 # 获取luci-app-argon-config
@@ -34,8 +34,6 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git package/argon-c
 # 获取灵缇加速器
 # git clone https://github.com/BCYDTZ/LingTiGameAcc.git package/lingtigame
 # git clone https://github.com/BCYDTZ/luci-app-LingTiGameAcc.git package/luci-app-lingtigame
-# 获取广告屏蔽大师plus
-# git clone https://github.com/small-5/luci-app-adblock-plus.git package/adblockplus
 # 获取KoolProxyR Plus+
 git clone https://github.com/tcsr200722/luci-app-ledeproxy-2.git package/ledeproxy
 # 获取高级设置
